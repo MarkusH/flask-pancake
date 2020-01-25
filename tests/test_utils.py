f2a1e9898b2140f24c2b5f345aa7baff69a5dad0
Markus Holtermann <info@markusholtermann.eu>
2020-01-25 21:20:41 +0100

    Add import utility tests

---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import pytest
+
+from flask_pancake.utils import import_from_string
+
+
+def some_func() -> int:
+    return 123
+
+
+def test_import_from_string_attr():
+    func = import_from_string("tests.test_utils:some_func")
+    assert func is some_func
+    assert func() == 123
+
+
+def test_import_from_string_attr_not_found():
+    with pytest.raises(AttributeError):
+        assert import_from_string("tests.test_utils:DOES_NOT_EXIST")
+
+
+def test_import_from_string_module():
+    from tests import test_utils
+
+    assert import_from_string("tests.test_utils") is test_utils
+
+
+def test_import_from_string_module_not_found():
+    with pytest.raises(ModuleNotFoundError):
+        assert import_from_string("flask_pancake.DOES_NOT_EXIST")
